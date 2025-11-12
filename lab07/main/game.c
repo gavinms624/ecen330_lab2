a7d3cfb92492eda31876b13fe7eaca25d6a3bd21
gavinms624 <gavinsimpsonm@gmail.com>
2025-11-12 11:30:27 -0700

    start

---
+++ lab07/main/game.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h> // rand
+
+#include "hw.h"
+#include "lcd.h"
+#include "cursor.h"
+#include "pin.h"
+#include "game.h"
+
+
+// Initialize the game control logic.
+void game_init(void){
+
+}
+
+// Update the game control logic.
+void game_tick(void){
+
+}
